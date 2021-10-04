6e872fdfe3c938025f793d3323975231711a2e95
Thiago Gomes <dm.thiagogb@hotmail.com>
2021-10-04 18:09:54 -0300

    Initial commit

---
+++ lib/core/Screens/mainPage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+
+class mainPage extends StatefulWidget {
+  const mainPage({Key? key}) : super(key: key);
+
+  @override
+  _mainPageState createState() => _mainPageState();
+}
+
+class _mainPageState extends State<mainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
